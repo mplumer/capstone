838bf76ee39423c1f719a00bf038f8ad149996eb
Max Plumer <maxplumer12@gmail.com>
2022-07-05 15:01:54 -0400

    product card, shop page

---
+++ src/components/product-card/product-card.component.jsx
@@ -0,0 +1,20 @@
+import './product-card.styles.scss';
+
+import Button from '../button/button.component';
+
+const ProductCard = ({ product }) => {
+  const { title, price, imageUrl } = product;
+  return (
+    <div className="product-card-container">
+      <img src={imageUrl} alt={`${title}`} />
+      <div className="footer">
+        <span className="name">{title}</span>
+
+        <span className="price">{price}</span>
+      </div>
+      <Button buttonType="inverted">Add to cart</Button>
+    </div>
+  );
+};
+
+export default ProductCard;
