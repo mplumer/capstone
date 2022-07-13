f1020bc1ba07803e3ef4351f35488889c85cd225
Max Plumer <maxplumer12@gmail.com>
2022-07-13 15:28:46 -0400

    user and cart reducers

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 
 import {
   onAuthStateChangedListener,
@@ -10,8 +10,46 @@ export const UserContext = createContext({
   currentUser: null,
 });
 
+// Constant to hold values for user actions
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+// User reducer function
+// This function is used to update the state of the user context
+// It takes in the current state and an action object
+// The action object contains the type of action and the payload
+// The payload is the data that is being updated in the state
+// The type of action is used to determine what data is being updated
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled action type: ${type}`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (user) => {
+    dispatch({
+      type: USER_ACTION_TYPES.SET_CURRENT_USER,
+      payload: user,
+    });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
