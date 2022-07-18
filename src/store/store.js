94094daf8f8996ba3bb6e6566849f16e41186656
Max Plumer <maxplumer12@gmail.com>
2022-07-17 21:24:17 -0400

    redux logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,9 @@
-//import { compose, applyMiddleware } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
-
-// Configure the logger to log only when the state changes
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
-
-  next(action);
-
-  console.log('next state: ', store.getState());
-};
 
 // Configure how the store should be persisted
 const persistConfig = {
@@ -31,11 +16,14 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // Constant for logger configuration
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+  Boolean
+);
 
 // Create the store
 export const store = configureStore({
   reducer: persistedReducer,
+  devTools: process.env.NODE_ENV === 'development',
   middleware: middleWares,
 });
 
